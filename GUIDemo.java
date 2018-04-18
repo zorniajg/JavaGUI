f7fb551789137679a9c86b2f6715e99141bdf2eb
Maciej Sieńkowski <Maciej@MacBook-Pro-Maciej-2.local>
2018-04-18 14:57:43 -0400

    added GUIDemo.java into my first project

---
+++ GUIDemo.java
@@ -0,0 +1,60 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+/**
+ * Minimal Java Swing application.
+ * 
+ * @author Nathan Sprague
+ * 
+ */
+public class GUIDemo extends JFrame
+{
+    private JPanel panel;
+    private JButton biggerButton;
+    private JButton smallerButton;
+
+    /**
+     * Set up the application.
+     */
+    public GUIDemo()
+    {
+	// CONSTRUCTOR NEEDS TO BE FINISHED!
+    }
+
+    /**
+     * This inner class exists to handle button events. There are other ways
+     * this could have been done:
+     * 
+     * 1. GUIDemo could implement ActionListener itself. 
+     * 2. Anonymous inner classes could be used to hand the events.
+     */
+    private class ButtonHandler implements ActionListener
+    {
+
+        public void actionPerformed(ActionEvent e)
+        {
+            Dimension size = getSize();
+
+            if (e.getSource().equals(biggerButton))
+            {
+                setSize(size.width + 10, size.height + 10);
+            }
+            else
+            {
+                setSize(size.width - 10, size.height - 10);
+            }
+
+        }
+    }
+
+    /**
+     * Start the app by creating a GUIDemo object.
+     */
+    public static void main(String[] args)
+    {
+        GUIDemo app = new GUIDemo();
+    }
+}
