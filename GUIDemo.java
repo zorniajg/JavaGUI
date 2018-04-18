fe03bf4260e82002b5187295071a32364983c4e1
Maciej Sieńkowski <Maciej@MacBook-Pro-Maciej-2.local>
2018-04-18 15:02:54 -0400

    updated GUIDemo constructor

--- GUIDemo.java
+++ GUIDemo.java
@@ -21,7 +21,18 @@ public class GUIDemo extends JFrame
      */
     public GUIDemo()
     {
-	// CONSTRUCTOR NEEDS TO BE FINISHED!
+	setTitle("Bigger/Smaller");
+        setSize(200, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new JPanel();
+        biggerButton = new JButton("BIGGER");
+        smallerButton = new JButton("SMALLER");
+        biggerButton.addActionListener(new ButtonHandler());
+        smallerButton.addActionListener(new ButtonHandler());
+        add(panel);
+        panel.add(biggerButton);
+        panel.add(smallerButton);
+        setVisible(true);
     }
 
     /**
